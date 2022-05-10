ec8454a62ca7df74f03cc4d81f820662a03184cd
fabionfd <fabionfd@gmail.com>
2022-05-10 03:36:53 -0300

    8080 no env

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const app = require('./app.js');
 
-var serverApp = app.listen(8080, '0.0.0.0',() => {
-// var serverApp = app.listen(process.env.PORT || 8081, '0.0.0.0',() => {
+var serverApp = app.listen(process.env.PORT || 8081, '0.0.0.0',() => {
     console.log('Torno-api executando na porta:', process.env.PORT || 8081)
 });
 
