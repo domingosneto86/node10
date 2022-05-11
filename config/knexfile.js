aa4b22a9aade290589e0cb99e934b7fbaae851cd
fabionfd <fabionfd@gmail.com>
2022-05-10 21:49:55 -0300

    console.log knexfile

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,13 +1,18 @@
+const connection = {
+	host : process.env.DB_HOST,
+	user : process.env.DB_USER,
+	password : process.env.DB_PASSWORD,
+	database : process.env.DB_NAME,
+	port: process.env.DB_PORT | 5432,
+	application_name: 'Tomo-Api'
+}
+
+console.log('conn', connection);
+console.log('db_max_pool', process.env.DB_MAX_POOL);
+
 module.exports = {
 	client: 'pg',
-	connection: {
-		host : process.env.DB_HOST,
-		user : process.env.DB_USER,
-		password : process.env.DB_PASSWORD,
-		database : process.env.DB_NAME,
-		port: process.env.DB_PORT | 5432,
-		application_name: 'Tomo-Api'
-	},
+	connection: connection,
 	pool: {
 		min: 2,
 		max: parseInt(process.env.DB_MAX_POOL, 10),
